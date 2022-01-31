fc57d60bb3ad74d01af1882f9b83ae4ee1d9111d
Julio-Usac <3300473961201@ingenieria.usac.edu.gt>
2022-01-30 20:06:52 -0600

    Practica 2

---
+++ Practicas/Practica 2/Practica2/src/analizadores/Nodo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package analizadores;
+
+/**
+ *
+ * @author Julio Garcia
+ */
+public class Nodo {
+    public int entero;
+    public String val;
+    public float decimal,aux;
+}
---
+++ Practicas/Practica 2/Practica2/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1;
+
+import java.io.FileInputStream;
+
+/**
+ *
+ * @author Julio Garcia
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+    
+        interpretar("Entrada.txt");
+   }
+    
+    private static void interpretar(String path) {
+        analizadores.Sintactico pars;
+        try {
+            pars=new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
+            pars.parse();        
+        } catch (Exception ex) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+ex.getCause());
+        } 
+    }
+    
+}
